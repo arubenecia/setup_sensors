2bfe694ad0aaa58b645c7676e1eb7d666c888781
arubenecia <82921981+arubenecia@users.noreply.github.com>
2021-07-19 18:42:08 +0800

    Update display.launch.py

--- sam_bot_description/launch/display.launch.py
+++ sam_bot_description/launch/display.launch.py
@@ -15,8 +15,7 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='sam_bot_description').find('sam_bot_description')
     default_model_path = os.path.join(pkg_share, 'src/description/sam_bot_description.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz'),
-    world_path=os.path.join(pkg_share, 'world/my_world2.sdf'),
-
+    world_path=os.path.join(pkg_share, 'world/my_world.sdf'),
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
